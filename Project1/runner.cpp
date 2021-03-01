91d1cc3ca75b1dadf60aaab181dbe4e784ff1ccd
MrzkR <mrzkr2ds@gmail.com>
2021-03-02 05:00:04 +0900

    reduced indent of namespace Runner

--- Project1/runner.cpp
+++ Project1/runner.cpp
@@ -4,57 +4,52 @@
 
 namespace Xscript
 {
-	namespace Runner
+	Value Runner::run_expr(Node *node)
 	{
-
-
-		Value run_expr(Node *node)
-		{
-			if( node == nullptr )
-				return { };
-
-			switch( node->type )
-			{
-				case Node::Type::Value:
-					return node->tok->value;
-
-				default:
-				{
-					Value lhs = run_expr(node->lhs);
-					Value rhs = run_expr(node->rhs);
-
-					switch( node->type )
-					{
-						case Node::Type::Add:
-						{
-							if( lhs.type == Value::Type::Array )
-							{
-								if( rhs.type == Value::Type::Array )
-									lhs.list += rhs.list;
-								else
-									lhs.list.push_back(rhs);
-
-								return lhs;
-							}
-							else if( rhs.type == Value::Type::Array )
-								Error(node->tok->pos, "type mismatch");
-
-							switch( lhs.type )
-							{
-								case Value::Type::Int:
-									lhs.v_Int += rhs.v_Int;
-									break;
-							}
-
-							break;
-						}
-					}
-
-					return lhs;
-				}
-			}
-
+		if( node == nullptr )
 			return { };
+
+		switch( node->type )
+		{
+			case Node::Type::Value:
+				return node->tok->value;
+
+			default:
+			{
+				Value lhs = run_expr(node->lhs);
+				Value rhs = run_expr(node->rhs);
+
+				switch( node->type )
+				{
+					case Node::Type::Add:
+					{
+						if( lhs.type == Value::Type::Array )
+						{
+							if( rhs.type == Value::Type::Array )
+								lhs.list += rhs.list;
+							else
+								lhs.list.push_back(rhs);
+
+							return lhs;
+						}
+						else if( rhs.type == Value::Type::Array )
+							Error(node->tok->pos, "type mismatch");
+
+						switch( lhs.type )
+						{
+							case Value::Type::Int:
+								lhs.v_Int += rhs.v_Int;
+								break;
+						}
+
+						break;
+					}
+				}
+
+				return lhs;
+			}
 		}
+
+		return { };
 	}
 }
