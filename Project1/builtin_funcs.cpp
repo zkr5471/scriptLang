a5ca069334084d4ec957a9c9ed08575e518509e4
MrzkR <mrzkr2ds@gmail.com>
2021-03-05 17:46:49 +0900

    add 'range' function

--- Project1/builtin_funcs.cpp
+++ Project1/builtin_funcs.cpp
@@ -18,6 +18,26 @@ namespace Xscript
 			return ret;
 		}
 
+		if( name == "range" )
+		{
+			if( node->list.size() != 1 )
+				Error(node->tok->pos, "illegal call function 'range'");
+
+			Value r = run_expr(node->list[0]);
+			if( r.type != Value::Type::Int )
+				Error(node->tok->pos, "function 'range' is required integer on param[0]");
+
+			ret.type = Value::Type::Array;
+			for( int i = 0; i < r.v_Int; i++ )
+			{
+				Value x;
+				x.v_Int = i;
+				ret.list.push_back(x);
+			}
+
+			return ret;
+		}
+
 		Error(node->tok->pos, "unknown function '" + name + "'");
 	}
 
